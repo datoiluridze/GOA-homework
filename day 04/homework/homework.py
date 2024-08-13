ccced3ec06435fec998fc739fdc45e19bec9d971
datoiluridze <davitiluridze45@gmail.com>
2024-08-13 14:07:31 +0400

    first commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,7 @@
+name = input ("enter name: ")
+# name = 'Ghoul'
+surname = input ("enter surname: ")
+# surname = 'Scott'
+age = input ("enter age: ")
+# age = '13'
+print ("hello", name, surname )
---
+++ day 11/homework/homework.py
@@ -0,0 +1,9 @@
+# 1
+first = int(input("Please enter first number: "))
+second = int(input("Please enter second number: "))
+
+print(first == second)
+
+# 2 
+age = int(input("Please enter your age: "))
+print(age > 18)
