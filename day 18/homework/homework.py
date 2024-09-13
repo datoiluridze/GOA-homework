22334bcf7021a90504a70472848ab4ede1d1a608
datoiluridze <davitiluridze45@gmail.com>
2024-09-13 10:42:55 -0700

    first commit

---
+++ day 18/homework/homework.py
@@ -0,0 +1,19 @@
+# 1)
+start = int(input("Please enter a start number: "))
+end = int(input("Please enter an end number: "))
+step = int(input("Please enter a step number: "))
+
+for i in range(start, end, step):
+    print(i)
+
+# 2)
+
+for i in range(14):
+    print("Dato")
+
+# 3)
+
+# 0-50
+
+for i in range(0, 50 + 1, 2):
+    print(i)
