c5eb431f50a82e168c3ab5c4243fba8376a72d97
datoiluridze <davitiluridze45@gmail.com>
2024-10-13 11:44:25 -0700

    fifth commit

--- day 14/classwork/classwork.py
+++ day 14/classwork/classwork.py
@@ -1,1 +1,6 @@
-print("GOA")
+my_age = 14
+
+user_input = int(input("enter your age: "))
+is_equal = (user_input == my_age)
+
+print(is_equal)
---
+++ day 24/classwork/classwork.py
@@ -0,0 +1,23 @@
+# 1)
+
+# format()
+name = "Saba"
+surname = "Janezashvili"
+
+print("Hello, I'm {} {}".format(name, surname))
+
+# 2)
+
+# joint()
+
+str_numbers = ["555","333","111"]
+
+print("-".join(str_numbers))
+
+# 3)
+
+# split()
+
+string1 = "Hello I'm Dato Iluridze"
+
+print(string1.split())
